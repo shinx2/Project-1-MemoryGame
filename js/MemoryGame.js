c65e8b9d84ed88ae51ef62d070126a8bb288a945
shinx2 <limkah_sh@hotmail.com>
2021-03-11 12:47:49 +0800

    updated

--- js/MemoryGame.js
+++ js/MemoryGame.js
@@ -1,8 +1,8 @@
 const cardArray = [ 
     {
-        name:"chase",
-        img: "chase.png",
-        front:"paw-patrol.png",
+        name:"professor",
+        img: "professor.png",
+        front:"MU.png",
     },
     {
         name:"eye",
@@ -15,9 +15,9 @@ const cardArray = [
         front:"MU.png",
     },
     {
-        name:"marshall",
-        img: "marshall.png",
-        front:"paw-patrol.png",
+        name:"two",
+        img: "two.png",
+        front:"MU.png",
     },
     {
         name:"mike",
@@ -30,196 +30,231 @@ const cardArray = [
         front:"MU.png",
     },
     {
-        name:"rocky",
-        img: "rocky.png",
-        front:"paw-patrol.png",
+        name:"bird",
+        img: "bird.png",
+        front:"MU.png",
     },
     {
-        name:"skye",
-        img: "skye.png",
-        front:"paw-patrol.png",
+        name:"miketwo",
+        img: "miketwo.png",
+        front:"MU.png",
     },
     {
         name:"strong",
         img: "strong.png",
         front:"MU.png",
-    },{
+    },
+    {
         name:"twin",
         img: "twin.png",
         front:"MU.png",
-    }
+    }, 
+    {
+        name:"logo",
+        img: "logo.png",
+        front:"MU.png",
+    }, 
+
 ]
 
-// shuffle cards from card array
-const selectNumOfImgs = (cardArray, num) => {
-    let shuffleCardArray = cardArray.sort(() => Math.random() - 0.5);
-    // console.log(shuffleCardArray.slice(0, num));
-    return shuffleCardArray.slice(0, num);
-}
 
-// duplicate the image chosen from shuffle card array
-const getPairs = (shuffleCardArray) => {
-    let getPairsArray = [];
-    for (let i = 0; i< shuffleCardArray.length; ++i)
-    {
-        getPairsArray.push(shuffleCardArray[i]);
-        getPairsArray.push(shuffleCardArray[i]);
+$(()=> {  
+    let sec = 0;
+    let name = "";
+    let level = 1;
+    let score = 0;
+    let numOfCards = 2;
+    let returnUnDuplicatedArray =[];
+    let duplicateCurrArray=[];
+    
+    //Shuffle cards from card array
+    const selectNumOfImgs = (cardArray, num) => {
+        let shuffleCardArray = cardArray.sort(() => Math.random() - 0.5);
+        // console.log(shuffleCardArray.slice(0, num));
+        return shuffleCardArray.slice(0, num);   
     }
-    // console.log(getPairsArray);
-    return getPairsArray.sort(() => Math.random() - 0.5);
-}
-
-// go to next level after all cards are matched
-// const nextLevel = () =>{
-//     numOfCards = numOfCardsForEachLevel + 1;
-// }
-
-let numOfCardsForEachLevel = [3, 4, 5];
-let numOfCards = 2;
-
-let returnUnDuplicatedArray = selectNumOfImgs(cardArray, numOfCards);
-let duplicateCurrArray = getPairs(returnUnDuplicatedArray);
-
-let level=0;
-// create a function that checks numofcards to set level and set number of cards
-// $(function loading() {
-//  if (numofcards====0) {
-//      numofcards =2;
-//  }
-// })
-
-//Using Jquery to create elements
-$(function() {
-    
-    $("<section>", {
-      class: "grid",
-    }).appendTo("#game"); 
-    duplicateCurrArray.forEach(item=>{
-    $("<div>", {
-        class: "card",
-        dataName: item.name,
-        "data-iscardmatch": "notmatch",
-            // isCardMatch: false,
-            // text: item.name,
-    }).append(
-    $("<div>", {
-        class: "front",
-        css: {
-        "background-image": `url(../img/${item.front})`,
-    }
-    })
-    ).append(
-    $("<div>", {
-        class: "back",
-        css: {
-        "background-image": `url(../img/${item.img}`,
-    }
-    })
-    ).appendTo("section");
-    })
-})
-
-let flippedCard = false; 
-let firstCard = "";
-let secondCard = "";
-let score =0;
-
-// add onclick listener to div with class="card"
-$(()=> {
-    $(".card").on("click", (event)=>{
-      $(event.currentTarget).toggleClass("selected");
-
-      // IF two cards selected
-      if ($(".selected").length === 2) {
-          if ($(".selected").first().attr("dataname") === 
-          $(".selected").last().attr("dataname")) {
-              console.log("match");
-              $(".selected").first().attr("data-iscardmatch", "match");
-              $(".selected").last().attr("data-iscardmatch", "match");
-              $(".selected").first().off("click");
-              $(".selected").last().off("click");
-              $(".selected").first().addClass("match");
-              $(".selected").last().addClass("match");
-              $(".selected").first().removeClass("selected");
-              $(".selected").last().removeClass("selected");
-              
-              // create score function and put here
-              if (score === 0 ){
-              $("#score").text(score + 100);
-              } else 
-                console.log("score increased!");
-                $("#score").text(score += 100);
-               
-
-          } else
-              console.log("not match");
-
-          
-
-      // if 1 card selected, do nothing
-      } else if ($(".selected").length <2){
-        //console.log($(".selected").last().attr("dataname"));
-
-      // if more than two cards selected, we remove selected class
-      }else {
-        //   $('.card [data-i   cardmatch=notmatch]').removeClass("selected");
-        let notmatching = $('.card').filter(function(){
-                    return $(this).attr('data-iscardmatch') == "notmatch"
-                });
-        notmatching.removeClass("selected");
-      }
-    //   console.log($(".match").length);
-    //console.log(duplicateCurrArray.length);
-
-    if (duplicateCurrArray.length === $(".match").length) {
-        
-        console.log("all cards are matched");
-        $("#complete").text("Congrats for clearing level 1!");
-        // function called here
-
-        
-    } 
-      
-    })
-})
-
-
-//Timer Function setting seconds to mintues
-let sec =0;
-function pad (val){ return val > 9 ? val: "0" + val }
-setInterval(function() {
-    $("#seconds").html(pad(++sec%60));
-    $("#minutes").html(pad(parseInt(sec/60,10)));
-    
-}, 1000);
-
-
-
-
-    
-
-
-    //     if ('currentLevel') === 5){
-    //         alert(`Congrats in clearing all levels!`)
-    //         // resetToLevel1();
-    //     } else{
-    //         let currentleveldisplay = parseInt(localStorage.getItem('currentLevel'));
-    //         alert(`Congrats in clearing level ${currentleveldisplay}`)
-    //         goToNextLevel();// goToNextLevel(); After alert, we increase level += 1
-    //         location.reload();
-            
-    //         // resetToLevel1();
-    //     }
-    //   }
-
-    
-    
-
-    
   
 
-          // $(event.currentTarget).attr("dataname");
-        // $(event.currentTarget).attr("dataname");
-        // $(`[dataname=${firstCard}]`).off("click");
-        // $(`[dataname=${firstCard}]`).removeClass("selected")
+    //Duplicate the image chosen from shuffle card array
+    const getPairs = (shuffleCardArray) => {
+        let getPairsArray = [];
+        for (let i = 0; i< shuffleCardArray.length; ++i) {
+            getPairsArray.push(shuffleCardArray[i]);
+            getPairsArray.push(shuffleCardArray[i]);
+        }
+        // console.log(getPairsArray);
+        return getPairsArray.sort(() => Math.random() - 0.5);
+    }
+  
 
+    const loadGameSection = () => {
+      //Assign array length based on levels
+      returnUnDuplicatedArray = selectNumOfImgs(cardArray, numOfCards);
+      duplicateCurrArray = getPairs(returnUnDuplicatedArray);
+      
+
+      //To display current level
+      // $("#nameinput").text("");
+      $("#level").text(level);
+      
+
+      //Create section with class 
+      $("<section>", {
+        class: "grid",
+      }).appendTo("#game"); 
+      duplicateCurrArray.forEach(item=>{
+      $("<div>", {
+          class: "card",
+          dataName: item.name,
+          "data-iscardmatch": "notmatch",
+      }).append(
+      $("<div>", {
+          class: "front",
+          css: {
+          "background-image": `url(../img/${item.front})`,
+      }
+      })
+      ).append(
+      $("<div>", {
+          class: "back",
+          css: {
+          "background-image": `url(../img/${item.img})`,
+      }
+      })
+      ).appendTo("section");
+      })
+  
+      $(".card").on("click", (event)=>{
+        $(event.currentTarget).toggleClass("selected");
+  
+
+        // If two cards selected
+        if ($(".selected").length === 2) {
+            if ($(".selected").first().attr("dataname") === 
+            $(".selected").last().attr("dataname")) {
+                // console.log("match");
+                // $(".selected").first().attr("data-iscardmatch", "match");
+                // $(".selected").last().attr("data-iscardmatch", "match");
+                $(".selected").first().off("click");
+                $(".selected").last().off("click");
+                $(".selected").first().addClass("match");
+                $(".selected").last().addClass("match");
+                $(".selected").first().removeClass("selected");
+                $(".selected").last().removeClass("selected");
+                
+                //Append score whenever two card matches
+                $("#score").text(score+=100);
+  
+            } else{
+                 console.log("not match");
+            }
+        //If 1 card selected, do nothing
+        } else if ($(".selected").length <2) {
+  
+        
+        //If more than two cards selected, remove selected class
+        } else {
+          let notMatching = $('.card').filter(function(){
+              return $(this).attr('data-iscardmatch') == "notmatch"
+            });
+              notMatching.removeClass("selected");
+        }
+      
+
+        //If all cards matches 
+      if (duplicateCurrArray.length === $(".match").length) {
+        $("#complete").text("Congrats for clearing the level!");
+        $(".next").show();
+
+          //When game complete 
+          if ( level === 5) {
+            $("#complete").hide();
+            $(".next").hide();
+            // Display message with score and time taken to complete game after 1 secs 
+            setTimeout(() => {
+            $(".main").hide();
+            $("#game").hide();
+            $("#table").hide();
+            $(".congrats").show().text(`Congrats for clearing the all the levels! Your score was ${score}! You took ${sec} seconds.`);
+            $(".playagain").show()
+            $(".playagain").click(() => {
+              resetDefault();
+              $(".playagain").hide();
+              $(".congrats").hide();
+            });
+            }, 1000);
+          }
+          //  else{
+          //   $("#complete").text("Congrats for clearing the level!");
+          // }
+      } 
+      })
+      }
+    
+
+      $(".next").on('click',() => {
+        $(".next").hide();
+          $("section").remove();
+          // Empty text at every level
+          $("#complete").text("");
+          numOfCards += 1;   
+          returnUnDuplicatedArray =[];
+          duplicateCurrArray=[];
+          level += 1;
+          loadGameSection();
+     });
+    
+
+    const resetDefault = () => {
+            $("section").remove();
+       
+       //Set everything to default. Hide game section and show main section
+       numOfCards = 2; 
+       level = 1
+       score = 0;
+       sec = 0;
+       returnUnDuplicatedArray =[];
+       duplicateCurrArray = [];
+       $("#complete").text("");
+       $("#score").text("");
+       $(".main").show();
+       $("#game").hide();
+       $("#table").hide();
+       $(".next").hide();
+       $(".playagain").hide();
+       loadGameSection();
+     }
+    
+
+       //On first page 
+        $("#game").hide();
+        $("#table").hide();
+        $(".next").hide();
+        $(".playagain").hide();
+        $("#start").on("click", () => {
+        //Set the input name to our name variable
+          name = $("#nameinput").val();
+        // console.log($("#nameinput").val())
+          $(".main").hide();
+          $("#game").show();
+          $("#table").show();
+          $(".next").hide();
+          $("#name").text(name);
+        })
+
+        loadGameSection();
+    
+
+    // Timer function
+    const time = (val) => { 
+      if (val > 9) {
+        return val
+      } else
+      return '0' + val}
+    setInterval(() => {
+      $("#seconds").html(time(++sec%60));
+      $("#minutes").html(time(parseInt(sec/60,10)));
+    }, 1000);
+  })
+  
+  
